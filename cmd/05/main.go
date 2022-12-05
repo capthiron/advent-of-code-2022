cf9cf3b1950b9d59297142c925f15572d45f03a6
capthiron <24925821+capthiron@users.noreply.github.com>
2022-12-05 11:41:48 +0100

    celebrate advent of code day 5 part 1

---
+++ cmd/05/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/capthiron/advent-of-code-2022/pkg/input"
+	stack "github.com/golang-ds/stack/linkedstack"
+)
+
+func main() {
+
+	inputLines, err := input.ReadInput()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Part 1:", part1(inputLines))
+	fmt.Println("Part 2:", part2(inputLines))
+}
+
+func part1(inputLines []string) string {
+
+	crateStacks, rules := parseInput(inputLines)
+
+	for rules.Size() != 0 {
+		rule, _ := rules.Pop()
+		for i := 0; i < rule.amount; i++ {
+			crateToMove, _ := crateStacks[rule.from].Pop()
+			crateStacks[rule.to].Push(crateToMove)
+		}
+	}
+
+	var topCrates string
+	for _, crateStack := range crateStacks {
+		topCrate, _ := crateStack.Pop()
+		topCrates += topCrate
+	}
+
+	return topCrates
+}
+
+func part2(inputLines []string) int {
+	return 1
+}
+
+func parseInput(inputLines []string) ([]stack.LinkedStack[string], stack.LinkedStack[rule]) {
+
+	var crateStacks []stack.LinkedStack[string]
+	rules := stack.New[rule]()
+	var rulesDone bool
+	var indexLineDone bool
+	for i := len(inputLines) - 1; i >= 0; i-- {
+		if rulesDone && !indexLineDone {
+			indexLineDone = true
+			continue
+		}
+
+		line := inputLines[i]
+
+		if line == "" {
+			rulesDone = true
+			continue
+		}
+
+		if rulesDone {
+
+			var cratesInLine []string
+			for i := 0; i <= len(line) - 3; i += 4 {
+
+				crate := line[i:i+3]
+				if len(strings.TrimSpace(crate)) == 0 {
+					cratesInLine = append(cratesInLine, "")
+					continue
+				}	
+
+				cratesInLine = append(cratesInLine, string(crate[1]))
+			}
+
+			if len(crateStacks) == 0 {
+				for range cratesInLine {
+					crateStacks = append(crateStacks, stack.New[string]())
+				}
+			}
+
+			for i, crate := range cratesInLine {
+				if crate != "" {
+					crateStacks[i].Push(crate)
+				}
+			}
+
+			continue
+		}
+
+		rules.Push(*ruleFrom(line))
+	}
+
+	return crateStacks, rules
+}
+
+type rule struct {
+	amount int
+	from int
+	to int
+}
+
+func ruleFrom(line string) *rule {
+  re := regexp.MustCompile(`\d+`) 
+  extraction := re.FindAllString(line, -1)
+	amount, _ := strconv.Atoi(extraction[0])
+	from, _ := strconv.Atoi(extraction[1])
+	to, _ := strconv.Atoi(extraction[2])
+	return &rule{amount, from - 1, to - 1}
+}
---
+++ cmd/05/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+var inputLines []string = []string{
+	"    [D]    ",
+	"[N] [C]    ",
+	"[Z] [M] [P]",
+	" 1   2   3 ",
+	"",
+	"move 1 from 2 to 1",
+	"move 3 from 1 to 3",
+	"move 2 from 2 to 1",
+	"move 1 from 1 to 2",
+}
+
+func TestPart1(t *testing.T) {
+	result := part1(inputLines)
+	if result != "CMZ" {
+		t.Errorf("expected part1 to return CMZ but was %v.", result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := part2(inputLines)
+	if result != 0 {
+		t.Errorf("expected part2 to return 0 but was %v.", result)
+	}
+}
